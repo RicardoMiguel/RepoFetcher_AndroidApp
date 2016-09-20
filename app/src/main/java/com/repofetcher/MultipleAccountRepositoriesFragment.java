4d5b1383322d364f3df9544fe90da7c289eeea30
Ricardo Soares <ricardomiguelsoares@hotmail.com>
2016-09-21 00:49:49 +0100

    MultipleAccountRepositoriesFragment is saving its state now.

--- app/src/main/java/com/repofetcher/MultipleAccountRepositoriesFragment.java
+++ app/src/main/java/com/repofetcher/MultipleAccountRepositoriesFragment.java
@@ -3,6 +3,7 @@ package com.repofetcher;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.text.TextUtils;
 import android.util.Log;
@@ -13,6 +14,7 @@ import android.view.ViewGroup;
 import com.controller.RepoListPageAdapter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ricar on 08/09/2016.
@@ -25,21 +27,30 @@ public class MultipleAccountRepositoriesFragment extends BaseFragment{
     private static final String TAG = MultipleAccountRepositoriesFragment.class.getName();
     @Nullable
     private String text;
-    @Nullable
     private ArrayList<Integer> servicesAlias;
+    private List<RepoListFragment> fragments;
 
     public MultipleAccountRepositoriesFragment() {
         super(R.layout.multiple_account_repos);
+        fragments = new ArrayList<>();
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView");
-        Bundle bundle = getArguments();
+        Bundle bundle = savedInstanceState != null ? savedInstanceState : getArguments();
         if(bundle != null){
             text = bundle.getString(TEXT, null);
             servicesAlias = bundle.getIntegerArrayList(SERVICE_ALIAS);
+            if(servicesAlias != null) {
+                for (int i = 0; i < servicesAlias.size(); i++) {
+                    Fragment fragment = getChildFragmentManager().getFragment(bundle, SERVICE_ALIAS + servicesAlias.get(i));
+                    if(fragment instanceof RepoListFragment){
+                        fragments.add(i, (RepoListFragment)fragment);
+                    }
+                }
+            }
         }
         return super.onCreateView(inflater, container, savedInstanceState);
     }
@@ -51,10 +62,24 @@ public class MultipleAccountRepositoriesFragment extends BaseFragment{
         ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewPager);
 
 
-        viewPager.setAdapter(new RepoListPageAdapter(this.getContext(), getChildFragmentManager(), workViewPagerData()));
+        viewPager.setAdapter(new RepoListPageAdapter(getChildFragmentManager(), this.getContext(), fragments, workViewPagerData()));
 
         TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState");
+        super.onSaveInstanceState(outState);
+        outState.putString(TEXT, text);
+        outState.putIntegerArrayList(SERVICE_ALIAS, servicesAlias);
+        for(int i = 0; i<servicesAlias.size();i++){
+            RepoListFragment fragment = fragments.get(i);
+            if(fragment != null){
+                getChildFragmentManager().putFragment(outState, SERVICE_ALIAS + servicesAlias.get(i), fragment);
+            }
+        }
     }
 
     @Override
