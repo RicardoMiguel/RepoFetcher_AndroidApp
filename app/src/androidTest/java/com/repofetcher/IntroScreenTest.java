3b05d3b89eb5872d2d2bb5c89ba80a1612c7c387
Ricardo Soares <ricardomiguelsoares@hotmail.com>
2016-10-18 20:30:38 +0100

    Improved clickLoginScreen_OpensLoginScreenUi method.

--- app/src/androidTest/java/com/repofetcher/IntroScreenTest.java
+++ app/src/androidTest/java/com/repofetcher/IntroScreenTest.java
@@ -12,7 +12,10 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.Matchers.not;
 
 /**
  * Created by ricar on 18/10/2016.
@@ -30,6 +33,8 @@ public class IntroScreenTest {
     public void clickLoginScreen_OpensLoginScreenUi(){
         onView(withId(R.id.login_center_button)).perform(click());
 
-        onView(withId(R.id.login_github_button)).check(matches(isDisplayed()));
+        onView(withId(R.id.login_github_button)).check(matches(allOf(isDisplayed(), isEnabled())));
     }
+
+
 }
