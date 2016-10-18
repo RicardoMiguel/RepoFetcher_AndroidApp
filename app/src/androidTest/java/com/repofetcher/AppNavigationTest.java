4cb48b834965b39350ebf766b9ba52048f33184a
Ricardo Soares <ricardomiguelsoares@hotmail.com>
2016-10-18 22:55:09 +0100

    Created AppNavigationTest.

--- app/src/androidTest/java/com/repofetcher/AppNavigationTest.java
+++ app/src/androidTest/java/com/repofetcher/AppNavigationTest.java
@@ -3,19 +3,22 @@ package com.repofetcher;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.KeyEvent;
+import android.widget.EditText;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressKey;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.core.AllOf.allOf;
-import static org.hamcrest.Matchers.not;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by ricar on 18/10/2016.
@@ -23,18 +26,19 @@ import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class IntroScreenTest {
+public class AppNavigationTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mainActivityTestRule =
             new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void clickLoginScreen_OpensLoginScreenUi(){
-        onView(withId(R.id.login_center_button)).perform(click());
+    public void clickSearch_OpenRepositoriesUi(){
+        onView(withId(R.id.action_search)).perform(click());
 
-        onView(withId(R.id.login_github_button)).check(matches(allOf(isDisplayed(), isEnabled())));
+        onView(withId(android.support.design.R.id.search_src_text)).perform(typeText("RicardoMiguel"), pressKey(KeyEvent.KEYCODE_ENTER));
+
+        onView(withId(R.id.tabLayout)).check(matches(isDisplayed()));
+
     }
-
-
 }
