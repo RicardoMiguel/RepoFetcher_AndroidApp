7425155189b1d2f3d72d28bc3c220814085a3542
Ricardo Soares <ricardomiguelsoares@hotmail.com>
2016-11-02 15:09:31 +0000

    Created LoginCenterTest for Unit testing.

---
+++ app/src/test/java/com/repofetcher/LoginCenterTest.java
@@ -0,0 +1,65 @@
+package com.repofetcher;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+import com.controller.LoginCenterContract;
+import com.controller.LoginCenterController;
+import com.service.FetcherCallsHandler;
+import com.service.holder.RepoServiceType;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by ricar on 02/11/2016.
+ */
+
+public class LoginCenterTest {
+
+    @Mock
+    private LoginCenterContract.View view;
+
+    @Mock
+    private Resources resources;
+
+    @Mock
+    private Context context;
+
+    private LoginCenterContract.Controller controller;
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+
+        FetcherCallsHandler.init(context);
+
+        controller = new LoginCenterController(view, resources);
+    }
+
+    @Test
+    public void showNoSessionsView(){
+        controller.activeSessions();
+
+        verify(view).showNoSessionsView();
+    }
+
+    @Test
+    public void addGitHubSession_OpensWebView(){
+        controller.addSession(0);
+        verify(view).goToWebViewFragment(RepoServiceType.GITHUB);
+    }
+
+    @Test
+    public void addBitbucketSession_OpensWebView(){
+        controller.addSession(1);
+
+        verify(view).goToWebViewFragment(RepoServiceType.BITBUCKET);
+    }
+
+
+}
