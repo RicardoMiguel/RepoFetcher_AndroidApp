efd9ad768d120eeface06bf31cf72b62a6397f1b
Ricardo Soares <ricardomiguelsoares@hotmail.com>
2016-11-02 12:36:59 +0000

    Created tests for Generic errors.

---
+++ app/src/test/java/com/repofetcher/GenericErrorsTest.java
@@ -0,0 +1,51 @@
+package com.repofetcher;
+
+import android.accounts.NetworkErrorException;
+
+import com.controller.ErrorUIController;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by ricar on 01/11/2016.
+ */
+
+public class GenericErrorsTest {
+
+    @Mock
+    private ErrorsContract.View view;
+
+    private ErrorsContract.Controller controller;
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+        controller = new ErrorUIController(view);
+    }
+
+    @Test
+    public void showNetworkError(){
+        controller.handleError(new NetworkErrorException());
+
+        verify(view).showNetworkErrorView();
+    }
+
+    @Test
+    public void showUnexpectedError(){
+        controller.handleError(new IllegalAccessException());
+
+        verify(view).showUnexpectedErrorView();
+    }
+
+    @Test
+    public void retryClick(){
+        controller.onRetryButtonClick();
+
+        verify(view).retry();
+    }
+}
